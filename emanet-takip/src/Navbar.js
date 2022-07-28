66b9d70ad78ea88f29d7d206522a96259a9162d9
tgbsrce <101837527+tgbsrce@users.noreply.github.com>
2022-07-28 20:19:49 +0300

    Logo

--- emanet-takip/src/Navbar.js
+++ emanet-takip/src/Navbar.js
@@ -4,7 +4,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <>
-        <div className="jumbotrom p-3 p-md-5 text-white bg-dark" style={{backgroundImage: `url(${process.env.PUBLIC_URL+'/img/bg-desktop-light2.jpg'})`}}>
+        <div className="jumbotrom p-3 p-md-5 text-white bg-dark" style={{backgroundImage: `url(${process.env.PUBLIC_URL+'/img/purple.jpg'})`}}>
           <div className="container text-center">
           <img src="/img/logo-corporate-21.png"  width={500} alt="zafer-logo" className=""></img>
           </div>         
